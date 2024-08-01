c49109614c3e9fd064ca5facd9d8586bcf14f3b8
김민진 <kminjin99@naver.com>
2024-08-01 15:05:11 +0900

    fix(study): app.js 깃 머지 오류 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,8 @@
 import "./App.css";
 
-import MainPage from "./pages/MainPage";
-import StartPage from "./templates/StartTemplates";
-import { Routes, Route } from "react-router-dom";
-
-
-import QNApage from './templates/Board/QnaTemplate';
-import QNAWritePage from './templates/Board/QnaWriteTemplate';
-import QnaDetailTemplate from './templates/Board/QnaDetailTemplate';
-import QnaManagerDetail from './templates/Board/QnaManagerDetail';
-import AnnouncementDetail from './templates/Board/AnnouncementDetail';
-import AnnouncementWrite from './templates/Board/AnnouncementWrite';
-
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="board" element={<QNApage />}></Route>
-        <Route path="qna" element={<QNApage />}></Route>
-        <Route path="board/qna/detail/" element={<QnaDetailTemplate />}></Route>
-        <Route path="board/qna/manager" element={<QnaManagerDetail />}></Route>
-        <Route path="board/qna/write" element={<QNAWritePage />}></Route>
-        <Route
-          path="board/announcement/detail/"
-          element={<AnnouncementDetail />}
-        ></Route>
-        <Route
-          path="board/announcement/write"
-          element={<AnnouncementWrite />}
-        ></Route>
-      </Routes>
-
-      {/* <QnaDetailTemplate /> */}
-      {/* <QnaManagerDetail /> */}
-      {/* <MainPage /> */}
-      {/* <QNApage /> */}
-      {/* <QNAWritePage /> */}
-
-
     </div>
   );
 }
