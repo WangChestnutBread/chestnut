7f80ccb4f773a7762fd6e14e5e0600f850cd4137
김시현 <gus3260@naver.com>
2024-08-02 16:24:38 +0900

    fix-단순수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
-import Chapter2Detail from "./templates/StudyList/Chapter2Detail";
 
 function App() {
   return (
     <div className="App">
-      <Chapter2Detail />
+
     </div>
   );
 }
