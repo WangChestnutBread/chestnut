d817ee42208bcf8b787f136df45b584fafc2501f
eunji04 <eunjee0404@naver.com>
2024-08-03 04:20:43 +0900

    feat: 비밀번호 수정(회원 정보 수정시) 기능 개발

--- backend/src/main/java/com/chestnut/backend/member/controller/MemberController.java
+++ backend/src/main/java/com/chestnut/backend/member/controller/MemberController.java
@@ -1,14 +1,13 @@
 package com.chestnut.backend.member.controller;
 
 import com.chestnut.backend.common.dto.ResponseDto;
-import com.chestnut.backend.member.dto.FindIdResDTO;
-import com.chestnut.backend.member.dto.SignupReqDTO;
-import com.chestnut.backend.member.dto.FindIdReqDTO;
+import com.chestnut.backend.member.dto.*;
 import com.chestnut.backend.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -39,6 +38,13 @@ public class MemberController {
     @GetMapping("/check-loginId")
     public ResponseEntity<?> checkLoginIdDuplicate(@RequestParam String loginId) {
         memberService.checkLoginIdDuplicate(loginId);
+        return new ResponseEntity<>(new ResponseDto<>("200", null), HttpStatus.OK);
+    }
+
+    @PostMapping("/reset-pwd")
+    public ResponseEntity<?> resetPwd(@AuthenticationPrincipal CustomMemberDetails customMemberDetails, @Valid @RequestBody ResetPwdReqDTO resetPwdReqDTO) {
+        ResetPwdDTO resetPwdDTO = new ResetPwdDTO(customMemberDetails.getLoginId(), resetPwdReqDTO);
+        memberService.resetPwd(resetPwdDTO);
         return new ResponseEntity<>(new ResponseDto<>("200", null), HttpStatus.OK);
     }
 
