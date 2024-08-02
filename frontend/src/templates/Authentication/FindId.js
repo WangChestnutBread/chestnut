22df8e139de83599ad83f7b7ded60a4d30f12c14
이재혁 <esoo963@naver.com>
2024-08-02 12:22:30 +0900

    fix(connect): axios 수정-2

--- frontend/src/templates/Authentication/FindId.js
+++ frontend/src/templates/Authentication/FindId.js
@@ -25,6 +25,24 @@ function FindId(){
         else{
             alert("당신의 정보를 잘못 입력하셨습니다.");
         }
+        axios.post("https://i11d107.p.ssafy.io/chestnutApi/member/find-id",{
+            memberName: name,
+            email: email
+        })
+        .then(response=>{
+            if(response.data.code==200){
+                alert(`당신의 아이디는 ${response.data.loginId}`);
+            }
+            else if(response.data.code==714){
+                alert("멤버 조회에 실패했습니다.")
+            }
+            else if(response.data.code==713){
+                alert("아이디와 이메일이 불일치합니다.");
+            }
+            else{
+                alert("알수 없는 오류가 발생했습니다.")
+            }
+        })
         
     };
     const GotoBack=()=>{
@@ -38,7 +56,7 @@ function FindId(){
                 <MemberLogo title={'ID 찾기'} />
                     <div style={{paddingLeft: 91, paddingRight: 91, paddingTop: 48, paddingBottom: 48, background: '#DCB78F', borderRadius: 25, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 27, display: 'flex'}}>
                         <div style={{height: 246, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 14, display: 'flex'}}>
-                        <LoginIdInput title={'ID'} value={name} content={'아이디를 입력하세요'} work={handleChangeName}/>
+                        <LoginIdInput title={'이름'} value={name} content={'이름을 입력하세요'} work={handleChangeName}/>
                         <LoginIdInput title={'이메일'} value={email} content={'이메일을 입력하세요'} work={handleChangeEmail}/>
                         </div>
                         <div style={{height: 108, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 27, display: 'flex'}}>
