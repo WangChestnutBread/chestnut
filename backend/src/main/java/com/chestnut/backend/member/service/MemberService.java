321e497064cd2cb0c0befc9fe2596d0157c35860
eunji04 <eunjee0404@naver.com>
2024-08-02 15:06:20 +0900

    fix: 확인용 오류 메시지 추가

--- backend/src/main/java/com/chestnut/backend/member/service/MemberService.java
+++ backend/src/main/java/com/chestnut/backend/member/service/MemberService.java
@@ -43,6 +43,7 @@ public class MemberService {
         } catch (DataAccessException e) {
             throw new DatabaseException();
         } catch (Exception e) {
+            System.out.println(e.getMessage());
             throw new UnknownException();
         }
 
