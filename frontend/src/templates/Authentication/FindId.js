ee7f7d64f26b723a1ea6e287c266f218c07f5df6
이재혁 <esoo963@naver.com>
2024-08-02 12:42:54 +0900

    fix(findid): 아이디 찾기 후 로그인 페이지로 이동으로 수정

--- frontend/src/templates/Authentication/FindId.js
+++ frontend/src/templates/Authentication/FindId.js
@@ -26,6 +26,7 @@ function FindId(){
         .then(response=>{
             if(response.data.code==200){
                 alert(`당신의 아이디는 ${response.data.data.loginId}`);
+                navigate("/member/login");
             }
             else if(response.data.code==714){
                 alert("멤버 조회에 실패했습니다.")
