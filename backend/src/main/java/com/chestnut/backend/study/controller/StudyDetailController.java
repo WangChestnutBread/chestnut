5fafb7bccd21928583655029f0731a576890d61d
김아름 <rdf136@naver.com>
2024-08-03 23:19:29 +0900

    feat : 발음 평가 API 컨트롤러 추가

--- backend/src/main/java/com/chestnut/backend/study/controller/StudyDetailController.java
+++ backend/src/main/java/com/chestnut/backend/study/controller/StudyDetailController.java
@@ -2,12 +2,15 @@ package com.chestnut.backend.study.controller;
 
 import com.chestnut.backend.common.dto.ResponseDto;
 import com.chestnut.backend.study.dto.PronounceMethodDto;
+import com.chestnut.backend.study.dto.PronunceEvaluateDto;
 import com.chestnut.backend.study.dto.WordPronounceDto;
+import com.chestnut.backend.study.service.PronounceEvaluateService;
 import com.chestnut.backend.study.service.StudyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
@@ -18,6 +21,7 @@ import java.util.Map;
 public class StudyDetailController {
 
     private final StudyService studyService;
+    private final PronounceEvaluateService pronounceEvaluateService;
 
     @GetMapping("/{studyId}/word")
     public ResponseEntity<?> wordInfo(@PathVariable("studyId") Long studyId) {
@@ -33,4 +37,10 @@ public class StudyDetailController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+    @PostMapping("/pronunciation/evaluate")
+    public ResponseEntity<?> checkPronunciation(@RequestParam("word") String word,
+                                                @RequestParam("audio") MultipartFile audioFile) {
+        PronunceEvaluateDto evaluation = pronounceEvaluateService.pronounceEvaluate(word, audioFile);
+        return new ResponseEntity<>(new ResponseDto<>("200", evaluation), HttpStatus.OK);
+    }
 }
