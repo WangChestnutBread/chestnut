639fa470de7ccbee737b8a3d32248b92e405b5df
eunji04 <eunjee0404@naver.com>
2024-08-03 15:47:40 +0900

    fix: 비밀번호 수정 로직

--- backend/src/main/java/com/chestnut/backend/member/dto/ResetPwdDTO.java
+++ backend/src/main/java/com/chestnut/backend/member/dto/ResetPwdDTO.java
@@ -1,7 +1,6 @@
 package com.chestnut.backend.member.dto;
 
 import com.chestnut.backend.member.controller.ResetPwdReqDTO;
-import com.chestnut.backend.member.entity.Member;
 import lombok.Getter;
 
 @Getter
@@ -17,23 +16,6 @@ public class ResetPwdDTO {
         this.password = resetPwdReqDTO.getPassword();
         this.newPassword = resetPwdReqDTO.getNewPassword();
         this.newPasswordCheck = resetPwdReqDTO.getNewPasswordCheck();
-    }
-
-    public Member toEntity(Member member, String codePwd) {
-        return Member.builder()
-                .memberId(member.getMemberId())
-                .loginId(member.getLoginId())
-                .email(member.getEmail())
-                .password(codePwd)
-                .nickname(member.getNickname())
-                .memberName(member.getMemberName())
-                .isAdmin(member.isAdmin())
-                .avatar(member.getAvatar())
-                .reward(member.getReward())
-                .joinAt(member.getJoinAt())
-                .isWithdraw(member.isWithdraw())
-                .ranking(member.getRanking())
-                .build();
     }
 
 }
