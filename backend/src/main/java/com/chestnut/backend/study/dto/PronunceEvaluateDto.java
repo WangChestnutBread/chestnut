d0536072d79657d74df4adbcabce797517544deb
김아름 <rdf136@naver.com>
2024-08-03 17:02:34 +0900

    feat : PronunceEvaluateDto 개발

---
+++ backend/src/main/java/com/chestnut/backend/study/dto/PronunceEvaluateDto.java
@@ -0,0 +1,18 @@
+package com.chestnut.backend.study.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class PronunceEvaluateDto {
+    private int isPass;
+    private List<Integer> answerMismatchIndices;
+    private List<Integer> userMismatchIndices;
+}
