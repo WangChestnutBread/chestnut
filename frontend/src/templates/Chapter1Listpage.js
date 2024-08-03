397de19eb1dc44edd9040fb85ce0fb223ae861a4
이재혁 <esoo963@naver.com>
2024-08-04 00:30:04 +0900

    feat(connect):학습 목록페이지 연결

--- frontend/src/templates/Chapter1Listpage.js
+++ frontend/src/templates/Chapter1Listpage.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ChapterList from "../molecules/StudyList/ChapterList";
+import useAuthStore from "../stores/authStore";
+import axios from "axios";
 function Chapter1Listpage(){
+    const setAccessToken = useAuthStore((state) => (state.setAccessToken))
+    
     return(
         <ChapterList title={'CH1. 자음 / 모음'} />
     );
--- frontend/src/templates/StudyList.js
+++ frontend/src/templates/StudyList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import NavBar from "../organisms/NavBar";
 import "./StudyList.css"
 import ChapterMenu from "../atoms/ChapterMenu";
@@ -6,8 +6,34 @@ import {useNavigate} from "react-router-dom";
 import "./NavbarExample.css"
 import StudyBackButton from "../molecules/StudyBackButton"
 import ChestNutButton from "../organisms/ChestNutButton"
+import useAuthStore from "../stores/authStore";
+import axios from "axios";
+
 function StudyList(){
     const navigate=useNavigate();
+    const [data, setdata]=useState();
+    const setAccessToken = useAuthStore((state) => (state.setAccessToken));
+    axios.get("https://i11d107.p.ssafy.io/chestnutApi/study/chapter",{
+        headers:{
+            access: setAccessToken
+        }
+    }).then(response=>{
+        if(response.data.code==200){
+            setdata(response.data.data);
+        }
+        else if(response.data.code==801){
+            alert("유효하지 않은 토큰입니다.");
+        }
+        else if(response.data.code==710){
+            alert("DB에 없는 정보입니다.");
+        }
+        else if(response.data.code==299){
+            alert("알 수 없는 오류입니다.");
+        }
+        console.log(response);
+    }).catch(error=>{
+        console.log(error);
+    })
     const chapter1=()=>{
         navigate("/chapter1");
     };
@@ -53,29 +79,29 @@ function StudyList(){
                             <div className="group-box">
                                 <div className="cardgroup">
                                     <div className="cardlist">
-                                        <ChapterMenu title={'CH1. 자음 /모음'} work={chapter1}/>
+                                        <ChapterMenu title={`CH${data[0].chapterId}. ${data[0].chapterName}`} work={chapter1}/>
                                     </div>
                                     <div className="cardlist">
-                                        <ChapterMenu title={'CH2. 한 글자'} work={chapter2}/>
+                                        <ChapterMenu title={`CH${data[1].chapterId}. ${data[1].chapterName}`} work={chapter2}/>
                                     </div>
                                     <div className="cardlist">
-                                        <ChapterMenu title={'CH3. 받침글자'} work={chapter3}/>
+                                        <ChapterMenu title={`CH${data[2].chapterId}. ${data[2].chapterName}`} work={chapter3}/>
                                     </div>
                                     <div className="cardlist">
-                                        <ChapterMenu title={'CH4. 음운변동'} work={chapter4}/>
+                                        <ChapterMenu title={`CH${data[3].chapterId}. ${data[3].chapterName}`} work={chapter4}/>
                                     </div>
                                 </div>
                             </div>
                             <div className="cardgroup-sec">
                                 <div className="group-box">
                                     <div className="cardlist">
-                                        <ChapterMenu title={'CH5. 단어'} work={chapter5}/>
+                                        <ChapterMenu title={`CH${data[4].chapterId}. ${data[4].chapterName}`} work={chapter5}/>
                                     </div>
                                     <div className="cardlist">
-                                        <ChapterMenu title={'CH6. 문장'} work={chapter6}/>
+                                        <ChapterMenu title={`CH${data[5].chapterId}. ${data[5].chapterName}`} work={chapter6}/>
                                     </div>
                                     <div className="cardlist">
-                                        <ChapterMenu title={'CH7.'}/>
+                                        <ChapterMenu title={`CH${data[6].chapterId}. ${data[6].chapterName}`}/>
                                     </div>
                                 </div>
                             </div>
