2ec9eb2d623d98696c562c3fe797698b0b31e3ea
김아름 <rdf136@naver.com>
2024-08-03 23:28:16 +0900

    fix : 평가DTO 사용자 발음(pronunciation) 추가

--- backend/src/main/java/com/chestnut/backend/study/dto/PronunceEvaluateDto.java
+++ backend/src/main/java/com/chestnut/backend/study/dto/PronunceEvaluateDto.java
@@ -13,6 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 public class PronunceEvaluateDto {
     private int isPass;
+    private String pronunciation;
     private List<Integer> answerMismatchIndices;
     private List<Integer> userMismatchIndices;
 }
