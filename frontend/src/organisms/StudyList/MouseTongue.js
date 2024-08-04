46eb3860444d9699e4abe961b449abf5aa341712
kimsihyeon24 <gus3260@naver.com>
2024-08-04 10:47:33 +0900

    fix: access토큰 가져옴

--- frontend/src/organisms/StudyList/MouseTongue.js
+++ frontend/src/organisms/StudyList/MouseTongue.js
@@ -2,28 +2,22 @@ import { useEffect, useState } from "react";
 import baseApi from "../../api/fetchAPI";
 import Mouse from "../../molecules/StudyList/Mouse";
 import Tongue from "../../molecules/StudyList/Tongue";
-import { useParams } from "react-router-dom";
+import { json, useParams } from "react-router-dom";
 import useAuthStore from "../../stores/authStore";
 
 const MouseTongue = () => {
   const [mouthData, setMouthData] = useState("");
   const [tongueData, setTongueData] = useState("");
   const { studyId } = useParams();
-  const accessToken = useAuthStore((state) => ({
-    accessToken: state.accessToken,
-  }));
   useEffect(() => {
-    console.log(accessToken.accessToken)
-    baseApi
-      .get(`/study/detail/1/image`, {
-        headers: { Authorization: `Bearer ${accessToken.accessToken}` },
-      })
-      .then((res) => {
-        setMouthData(res.data.mouthImg);
-        setTongueData(res.data.tongueImg);
-        console.log(mouthData, tongueData);
-      });
-  });
+      baseApi.get(`/study/detail/1/image`).then((res) => {
+      setMouthData(res.data.mouthImg);
+      setTongueData(res.data.tongueImg);
+      console.log(res.data.data.tongueImg);
+      
+      
+    });
+  }, []);
 
   return (
     <div className="d-flex m-t-box">
