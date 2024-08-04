89692a82f98928f03c276fb212f367186012a816
이재혁 <esoo963@naver.com>
2024-08-04 12:12:25 +0900

    fix(list): 학습 list input data

--- frontend/src/templates/StudyList.js
+++ frontend/src/templates/StudyList.js
@@ -30,7 +30,7 @@ function StudyList() {
       })
       .then((response) => {
         if (response.data.code === 200) {
-          setData([response.data.data]);
+          setData((prevList)=>[...prevList, response.data.data]);
         } else if (response.data.code === 801) {
           alert("유효하지 않은 토큰입니다.");
         } else if (response.data.code === 710) {
