089f13eb35b1e345d416a4d5776fc6ea8913224b
김아름 <rdf136@naver.com>
2024-08-05 17:53:41 +0900

    feat : MainMemberInfoDto 구현

---
+++ backend/src/main/java/com/chestnut/backend/member/dto/MainMemberInfoDto.java
@@ -0,0 +1,26 @@
+package com.chestnut.backend.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class MainMemberInfoDto {
+    private Byte avatarId;
+    private String avatarName;
+    private String avatarImgUrl;
+    private Byte lowerLimit;
+    private String nickname;
+    private long reward;
+    private int ranking;
+    private Long studyId;
+    private String word;
+    private String chapterName;
+    private Short attendanceCount;
+
+    public void addHostUrl(String hostUrl){
+        this.avatarImgUrl = new StringBuilder().append(hostUrl).append(this.avatarImgUrl).toString();
+    }
+}
