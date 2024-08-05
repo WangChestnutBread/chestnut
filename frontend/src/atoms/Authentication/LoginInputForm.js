efcd47bb22870720d3742aef1056ba3754c5b6fb
이재혁 <esoo963@naver.com>
2024-08-05 14:21:41 +0900

    fix(studylist): 목록 글자 표시 및 링크 구현

--- frontend/src/atoms/Authentication/LoginInputForm.js
+++ frontend/src/atoms/Authentication/LoginInputForm.js
@@ -14,7 +14,7 @@ function LoginInputForm(props){
     };
     return(
 
-        <form className="FormBorder LoginFormFont"  onSubmit={handleSubmit}>
+        <form className="FormBorder LoginFormFont"  >
             <input className="LoginFormBorder LoginFormFont" type="text" value={props.name} onChange={props.work} placeholder={props.content}/>
         </form>
     );
--- frontend/src/atoms/BlackBoard.js
+++ frontend/src/atoms/BlackBoard.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./BlackBoard.css";
+
+// function Word({ number }) {
+
+
+//   return (
+//     <div>
+//       <b>{number}</b>
+//     </div>
+//   );
+// }
+
 function BlackBoard(props) {
+  // const workItems = props.word || [];
+  // const [data1, setData1] = useState([]);
+  // const [data2, setData2] = useState("");
+  // workItems.map((number, idx)=>(
+  //   if(number.categoryContent == 1) {
+  //     setData1(workItems.word);
+  // }
+  // ))
+  
   return (
     <div className="container">
       <div className="BlackBoardBox">
         <div className="BoardBox">
           <div className="ChapterContent">
-            {props.work.map(
-              (number, idx)=>{
-                <div>{number.data.word}</div>
-              }
-            )}
+            {/* <div>자음</div>
+            {data1.map((number, idx) => (
+              <Word number={number} key={idx} />
+            ))} */}
           </div>
           <div className="ChapterContent">
             {props.content}
           </div>
           <div className="greenBox">
-            <img className="pencil" src="/image/pencil.png" />
-            <img className="eraser" src="/image/ERASER.png" />
+            <img className="pencil" src="/image/pencil.png" alt="Pencil" />
+            <img className="eraser" src="/image/ERASER.png" alt="Eraser" />
           </div>
           <div className="brownBar" />
         </div>
@@ -25,4 +44,5 @@ function BlackBoard(props) {
     </div>
   );
 }
+
 export default BlackBoard;
