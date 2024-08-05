fa66ac436a878e372b4a4a9e63d5ffde135c3d15
김민진 <kminjin99@naver.com>
2024-08-05 11:01:51 +0900

    fix(study): 칠판 왼쪽 버튼 런타임 오류 수정

--- frontend/src/pages/StudyDetail/Ch4n7DetailPage.js
+++ frontend/src/pages/StudyDetail/Ch4n7DetailPage.js
@@ -3,34 +3,34 @@ import Chapter4n7Detail from "../../templates/StudyList/Chapter4n7Detail";
 import baseApi from "../../api/fetchAPI";
 import { useParams } from "react-router-dom";
 
-
 function Ch4n7DetailPage() {
-    let [data, setData] = useState(null);
-    
-    let {chapterId} = useParams();
+  let [data, setData] = useState(null);
 
-    console.log(chapterId)
-    useEffect(()=>{
-        baseApi({
-            method: 'get',
-            url: `/study/chapter/${chapterId}`
-        })
-        .then((res)=> {
-            setData(res.data.data)
-        })
-        .catch((err)=> {
-            console.log(err)
-        })
-    }, [])
-    
+  let { chapterId } = useParams();
 
-    return (
-        <div>
-            {
-                data ? <Chapter4n7Detail data={data} chapterId={chapterId}/> : <p>로딩중입니다</p>
-            }
-        </div>
-    )
+  console.log(chapterId);
+  useEffect(() => {
+    baseApi({
+      method: "get",
+      url: `/study/chapter/${chapterId}`,
+    })
+      .then((res) => {
+        setData(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      {data ? (
+        <Chapter4n7Detail data={data} chapterId={chapterId} />
+      ) : (
+        <p>로딩중입니다</p>
+      )}
+    </div>
+  );
 }
 
 export default Ch4n7DetailPage;
