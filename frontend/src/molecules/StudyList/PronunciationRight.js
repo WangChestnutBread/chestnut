0ec7a781fe2fd6e588237d84b7aa17429a167641
김시현 <gus3260@naver.com>
2024-08-05 13:19:26 +0900

    fix:디테일 페이지 90%완료

--- frontend/src/molecules/StudyList/PronunciationRight.js
+++ frontend/src/molecules/StudyList/PronunciationRight.js
@@ -1,7 +1,8 @@
 import "./PronunciationRight.css";
 import Text64 from "../../atoms/Text64";
 
-const PronunciationRight = ({ data }) => {
+const PronunciationRight = ( data ) => {
+  console.log(data);
   return (
     <div className="right d-flex justify-content-center align-items-center">
       <Text64 text={data.data} />
