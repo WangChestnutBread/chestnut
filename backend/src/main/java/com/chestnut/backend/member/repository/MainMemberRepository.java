aae5b9f8d5f1ebfbd6f1f7d5627c450cbf6e5d52
김아름 <rdf136@naver.com>
2024-08-05 17:57:34 +0900

    refactor : 불필요한 import 삭제

--- backend/src/main/java/com/chestnut/backend/member/repository/MainMemberRepository.java
+++ backend/src/main/java/com/chestnut/backend/member/repository/MainMemberRepository.java
@@ -6,9 +6,6 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public class MainMemberRepository {
 
