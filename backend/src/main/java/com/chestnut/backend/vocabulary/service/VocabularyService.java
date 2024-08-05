8643a9718204caa5f22401f05a9ba07ef775bb0d
Elenaljh <hye9807@naver.com>
2024-08-05 16:41:38 +0900

    feat: 단어장 목록 챕터별 조회 기능 추가

--- backend/src/main/java/com/chestnut/backend/vocabulary/service/VocabularyService.java
+++ backend/src/main/java/com/chestnut/backend/vocabulary/service/VocabularyService.java
@@ -46,11 +46,15 @@ public class VocabularyService {
         vocabularyRepository.delete(vocabulary);
     }
 
-    public Page<VocabularyDto> getVocabularyList(String loginId, Integer page, Integer size) {
+    public Page<VocabularyDto> getVocabularyList(String loginId, Byte chapterId, Integer page, Integer size) {
         Member member = memberRepository.findByLoginId(loginId)
                 .orElseThrow(MemberNotFoundException::new);
         Pageable pageable = PageRequest.of(page, size, Sort.by("study.studyId").ascending());
-        return vocabularyRepository.findByMember(member, pageable);
+        if (chapterId == (byte)0) {
+            return vocabularyRepository.findByMember(member, pageable);
+        } else {
+            return vocabularyRepository.findByMemberAndChapter(member, chapterId, pageable);
+        }
         //orElse()와 orElseGet의 차이 -> 전자는 객체를 인자로 받고 후자는 함수형 인터페이스를 인자로 받는다
         //orElse는 값을 생성하여 orElseGet보다 비용이 크므로 최대한 사용을 피해야 한다
     }
