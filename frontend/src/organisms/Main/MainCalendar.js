0beaaf2c46d9b6a08e80be0bf5d8d44b4d45578e
김민진 <kminjin99@naver.com>
2024-08-09 03:03:56 +0900

    fix(main): 캘린더 범위 현재년도로 수정

--- frontend/src/organisms/Main/MainCalendar.js
+++ frontend/src/organisms/Main/MainCalendar.js
@@ -7,6 +7,7 @@ function MainCalendar({ attendance }) {
   const [attendDay, setAttendDay] = useState(attendance);
   const [value, onChange] = useState(new Date());
 
+  // 스탬프 찍기
   const stamp = ({ date, view }) => {
     const result = attendDay.map((attends) => {
       if (new Date(attends).toDateString() === date.toDateString()) {
@@ -29,8 +30,8 @@ function MainCalendar({ attendance }) {
         onChange={onChange}
         value={value}
         minDetail="month"
-        minDate={new Date("2024-01-01")}
-        maxDate={new Date("2024-12-31")}
+        minDate={new Date(`${new Date().getFullYear()}-01-01`)}
+        maxDate={new Date(`${new Date().getFullYear()}-12-31`)}
         next2Label={null}
         prev2Label={null}
         tileContent={stamp}
