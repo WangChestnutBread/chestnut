4321928305435fcf088d1e3041d5ed5a8d2fb4ff
김민진 <kminjin99@naver.com>
2024-08-08 13:20:46 +0900

    feat(main): 마지막 학습 챕터 아이디 비워 둠

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -12,37 +12,35 @@ function MainPage() {
   const reissueToken = () => {
     // axios.post("https://i11d107.p.ssafy.io/chestnutApi/member/reissue",{})
     // .then((res) => console.log(res))
-    baseApi.post('/member/reissue').then((res) => {
+    baseApi.post("/member/reissue").then((res) => {
       console.log(res);
-    })
+    });
   };
-  
-  //axios 요청 모음
-  let [profile, setProfile] = useState(null)
-  useEffect(()=>{
+
+  //axios 요청
+  let [profile, setProfile] = useState(null);
+  useEffect(() => {
     // 프로필 정보 요청
     baseApi({
-      method: 'get',
-      url: '/member/info/main'
+      method: "get",
+      url: "/member/info/main",
     })
-    .then((res)=>{
-      setProfile(res.data.data)
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
+      .then((res) => {
+        setProfile(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
     return () => {
-      setProfile(null)
-    }
-  }, [])
+      setProfile(null);
+    };
+  }, []);
 
   return (
     <div className="MainPage">
       <button onClick={reissueToken}>토큰 재발급 테스트</button>
-      {
-        profile ? <MainTemplate profile={profile}/> : <p>로딩중입니다</p>
-      }
+      {profile ? <MainTemplate profile={profile} /> : <p>로딩중입니다</p>}
     </div>
   );
 }
