09dd9e19033edf989c3fdcea293c886e7102f5e6
김민진 <kminjin99@naver.com>
2024-08-09 09:38:32 +0900

    fix(main): 첫 랜딩 시 403 에러 해결

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -10,82 +10,83 @@ function MainPage() {
   const { accessToken, setAccessToken } = useAuthStore((state) => ({
     ...state,
   }));
-  const reissueToken = () => {
-    // axios.post("https://i11d107.p.ssafy.io/chestnutApi/member/reissue",{})
-    // .then((res) => console.log(res))
-    baseApi.post("/member/reissue").then((res) => {
-      console.log(res);
-    });
-  };
-  
-  let [profile, setProfile] = useState(null);
-  let [attendance, setAttendance] = useState(null)
-  
-  const getProfile = () => {
-    return (
-      baseApi({
-        method: "get",
-        url: "/member/info/main",
-      })
-        .then((res) => {
-          setProfile(res.data.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-    )
-    }
+  // const reissueToken = () => {
+  //   // axios.post("https://i11d107.p.ssafy.io/chestnutApi/member/reissue",{})
+  //   // .then((res) => console.log(res))
+  //   baseApi.post("/member/reissue").then((res) => {
+  //     console.log(res);
+  //   });
+  // };
 
-  const getAttendance = () => {
-    return(
-      baseApi({
-        method: "get",
-        url: `/member/attendance?year=${new Date().getFullYear()}`,
-      })
-      .then((res) => {
-          setAttendance(res.data.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-    )
-  }
-  
+  let [profile, setProfile] = useState(null);
+  let [attendance, setAttendance] = useState(null);
+
+  const getProfile = async () => {
+    try {
+      const response = await axios.get(
+        "https://i11d107.p.ssafy.io/chestnutApi/member/info/main",
+        {
+          headers: { access: accessToken },
+        }
+      );
+      setProfile(response.data.data);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getAttendance = async () => {
+    try {
+      const response = await axios.get(
+        `https://i11d107.p.ssafy.io/chestnutApi/member/attendance?year=${new Date().getFullYear()}`,
+        {
+          headers: { access: accessToken },
+        }
+      );
+      setAttendance(response.data.data);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   //axios 요청
-  useEffect(()=>{
+  useEffect(() => {
     const fetchData = async () => {
-      if (accessToken) {
-        try {
-          await Promise.all([getProfile(), getAttendance()]);
-        } catch (err) {
-          console.error(err);
-        } 
+      // console.log(accessToken);
+      // if (accessToken) {
+      try {
+        const profileResponse = await getProfile();
+        const attendanceResponse = await getAttendance();
+
+        console.log(profileResponse);
+        console.log(attendanceResponse);
+      } catch (err) {
+        console.error(err);
       }
-      
+      // }
     };
-    fetchData();
-  }, [])
+    if (accessToken) {
+      fetchData();
+    }
+  }, [accessToken, setAccessToken]);
 
   return (
     <div className="MainPage">
       <button onClick={reissueToken}>토큰 재발급 테스트</button>
-      {
-        profile && attendance ? (
-          <MainTemplate profile={profile} attendance={attendance} />
-        ) : (
-          <p>새로고침을 한 번만 눌러주세요^-^</p>
-        )
-      }
+      {profile && attendance ? (
+        <MainTemplate profile={profile} attendance={attendance} />
+      ) : (
+        <p>로딩중입니다</p>
+      )}
 
       <div>
         {/* 오픈 채팅 버튼 */}
-        <OpenChatButton/>
+        <OpenChatButton />
 
         {/* 챗봇 버튼 */}
-
       </div>
-      
-
     </div>
   );
 }
