5d40a1f2600871cb6789a0377f9aa9d914a4ecfd
김민진 <kminjin99@naver.com>
2024-08-11 04:27:33 +0900

    feat: 칠판 위 드롭다운 버튼 구현

---
+++ frontend/src/molecules/TabDropDown.js
@@ -0,0 +1,18 @@
+import "./TabDropDown.css";
+import Dropdown from "react-bootstrap/Dropdown";
+
+function TabDropDown({ content, handleOnClick }) {
+  return (
+    <Dropdown.Menu className="TabDropdownMenu">
+      {content.map((item, i) => {
+        return (
+          <Dropdown.Item className="TabDropItem" onClick={handleOnClick}>
+            {item.categoryContent}
+          </Dropdown.Item>
+        );
+      })}
+    </Dropdown.Menu>
+  );
+}
+
+export default TabDropDown;
--- frontend/src/organisms/StudyChapter/Chapter5List.js
+++ frontend/src/organisms/StudyChapter/Chapter5List.js
@@ -1,6 +1,30 @@
-import React from "react";
+import "./Chapter5List.css";
+import { useState } from "react";
 import ChapterList from "../../molecules/StudyList/ChapterList";
-function Chapter5List() {
-  return <ChapterList title={"Ch5. 단어"} />;
+import TabDropDown from "../../molecules/TabDropDown";
+import { Dropdown } from "react-bootstrap";
+function Chapter5List({ content, chapterId }) {
+  console.log(content);
+  let [currentTopic, setCurrentTopic] = useState(content[0].categoryContent);
+
+
+  const handleOnClick = (event) => {
+    setCurrentTopic(event.target.innerText);
+  };
+  
+  return (
+    <div className="Chapter5List">
+      {/* 칠판 위 드롭다운 버튼 */}
+      <Dropdown className="TabDropDownButton">
+        <Dropdown.Toggle id="dropdown-basic" className="TabDropDownToggle">
+          {currentTopic}
+        </Dropdown.Toggle>
+        <TabDropDown content={content} handleOnClick={handleOnClick} />
+      </Dropdown>
+
+      {/* 칠판 */}
+      <ChapterList title={"Ch5. 단어"} />
+    </div>
+  );
 }
 export default Chapter5List;
