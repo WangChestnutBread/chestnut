75234dd561d1cf8d30b4248542a635501bd810ff
김민진 <kminjin99@naver.com>
2024-08-11 02:41:39 +0900

    feat(study): 챕터3 음절의 끝소리 법칙 예시 추가

--- frontend/src/organisms/StudyChapter/Chapter5List.js
+++ frontend/src/organisms/StudyChapter/Chapter5List.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ChapterList from "../../molecules/StudyList/ChapterList";
 function Chapter5List() {
-  return <ChapterList title={"CH5. 단어"} />;
+  return <ChapterList title={"Ch5. 단어"} />;
 }
 export default Chapter5List;
