dfd30131a8d0d36fc62d5dc88a4ae7b4261c8523
김민진 <kminjin99@naver.com>
2024-08-12 06:46:08 +0900

    feat(voca): 단어장 api 연결 구현중

--- frontend/src/organisms/VocabularyList.js
+++ frontend/src/organisms/VocabularyList.js
@@ -1,37 +1,69 @@
 import "./VocabularyList.css";
 import ChapterList from "../molecules/StudyList/ChapterList";
 import { useEffect, useState } from "react";
-import { ListGroup } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import baseApi from "../api/fetchAPI";
+import Pagenation from "../atoms/Pagenation";
+import Text32 from "../atoms/Text32";
 
-function VocabularyList({ chapterTitle, content }) {
-  const handleTabClick = (index) => {
-    setCurrentTab(index);
+function VocabularyList({
+  chapterTitle,
+  getVocabulary,
+  currentVocaItem,
+  setCurrentVocaItem,
+}) {
+  let [currentChapter, setCurrentChapter] = useState(0);
+  let [currentVocaPage, setCurrentVocaPage] = useState(1);
+  let totalPages = currentVocaItem.totalPages;
+
+  const handleOnVocaClick = (index) => {
+    setCurrentChapter(index);
+    setCurrentVocaPage(1);
   };
 
-  let [currentTab, setCurrentTab] = useState(0);
-  // console.log(content)
+  const handlePageChange = () => {
+    getVocabulary(currentChapter, currentVocaPage);
+  };
+
+  const handleUpPageChange = () => {
+    if (currentVocaPage < totalPages) {
+      getVocabulary(currentChapter, currentVocaPage + 1);
+    }
+  };
+
+  const handleDownPageChange = () => {
+    if (currentVocaPage > 1) {
+      getVocabulary(currentChapter, currentVocaPage - 1);
+    }
+  };
 
   return (
-    <div className="BlackBoardWithTab">
-      <div className="BlackBoardWithLine">
-        <div className="BlackBoardBody">
-          {/* 칠판 위 탭 버튼 */}
+    <div className="VocaList container">
+      {/* 칠판 위 드롭다운 버튼 */}
 
-          {/* 칠판 */}
-          <div className="B">
-            <ChapterList title="나의 단어장" />
-          </div>
+      {/* 칠판 안 */}
+      <div className="VocaListContent">
+        {currentVocaItem.content.map((item, i) => {
+          return (
+            <div className="VocaListWord">
+              <Text32 text={item.word} />
+            </div>
+          );
+        })}
+      </div>
 
-          {/* 칠판 안 */}
-          <div className="BoardInside">
-            {/* 칠판 내용 */}
-            <ListGroup variant="flush" className="BoardList">
-              {content.map((item) => {
-                return (<ListGroup.Item>{item.word}</ListGroup.Item>)
-              })}  
-            </ListGroup>
-          </div>
-        </div>
+      {/* 칠판 */}
+      <ChapterList title={"나의 단어장"} />
+
+      {/* 페이지네이션 */}
+      <div className="Chapter5_6Pagenation">
+        <Pagenation
+          currentPage={currentVocaPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+          upPageChange={handleUpPageChange}
+          downPageChange={handleDownPageChange}
+        />
       </div>
     </div>
   );
