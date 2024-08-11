2698955e8caf328719b4b08dff60a3acec85833e
김민진 <kminjin99@naver.com>
2024-08-11 22:52:38 +0900

    fix(study): 학습 전체 목록 페이지 수정 완료

--- frontend/src/templates/StudyList.js
+++ frontend/src/templates/StudyList.js
@@ -1,16 +1,17 @@
 import "./StudyList.css";
 import baseApi from "../api/fetchAPI";
-import ChapterMenu from "../atoms/ChapterMenu";
+import ChapterCard from "../atoms/ChapterCard";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./NavbarExample.css";
 import StudyBackButton from "../molecules/StudyBackButton";
 import ChestNutButton from "../organisms/ChestNutButton";
 import Text32 from "../atoms/Text32";
+import { Row, Col } from "react-bootstrap";
 
 function StudyList() {
   const navigate = useNavigate();
-  const [listdata, setlistData] = useState([]);
+  const [listdata, setListData] = useState([]);
   
   useEffect(() => {
     // if (!accessToken) {
@@ -23,16 +24,12 @@ function StudyList() {
       url: "/study/chapter",
     }) 
     .then((response) => {
-      console.log(response.data);
+      setListData(response.data.data);
     })
     .catch((error) => {
       console.log(error);
     });
   }, []);
-
-  const handleChapterNavigation = (chapter) => {
-    navigate(`/chapter/${chapter}`);
-  };
 
   return (
     <div>
@@ -46,47 +43,28 @@ function StudyList() {
           </div>
         </div>
       
+      {/* 챕터 카드 목록 */}
       <div className="container">
         {/* 제목 */}
         <div className="titleBox">
           <Text32 text={"무엇을 학습할까?"}/>
         </div>
 
-        챕터 리스트        
-        {/* <div className="chapterlist">
-          <div className="chapterTotalBox">
-            <div className="chapterinnerbox"> */}
-              {/* <div className="group-box">
-                <div className="cardgroup">
-                    {listdata.map((chapter) => (
-                        <div className="cardlist" key={chapter.chapterId}>
-                          <ChapterMenu
-                            title={`CH${chapter.chapterId}. ${chapter.chapterName}`}
-                            work={() => handleChapterNavigation(chapter.chapterId)}
-                          />
-                        </div>
-                      ))}
-                </div>
-              </div> */}
-              <div className="cardgroup-sec">
-                {/* <div className="group-box">
-                    {listdata.slice(4).map((chapter) => (
-                        <div className="cardlist" key={chapter.chapterId}>
-                          <ChapterMenu
-                            title={`CH${chapter.chapterId}. ${chapter.chapterName}`}
-                            work={() => handleChapterNavigation(chapter.chapterId)}
-                            chapterId={chapter.chapterId}
-                          />
-                        </div>
-                    ))}
-                </div> */}
-              {/* </div>
-            </div>
-          </div> */}
-        </div>
+        {/* 카드 목록 */}
+        <Row xs={2} md={3} lg={4} className="g-4 ChapterCardList">
+          {listdata.map((item, i) => (
+            <Col key={i}>
+             <ChapterCard content={item}/>
+            </Col>
+          ))}
+        </Row>
+
+        
       </div>
     </div>
   );
 }
 
 export default StudyList;
+
+
