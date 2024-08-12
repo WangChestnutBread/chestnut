57014b424689e42970c1c92fb60f02924daefc8e
Elenaljh <hye9807@naver.com>
2024-08-12 13:59:05 +0900

    feat: id찾기, 랜딩페이지 수정

--- frontend/src/templates/StartTemplates.js
+++ frontend/src/templates/StartTemplates.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./StartTemplates.css";
 
@@ -14,27 +14,27 @@ function LandingPage() {
     return () => clearTimeout(timer);
   }, []);
 
-
   const GotoLogin = () => {
     navigate("/member/login");
   };
   const GotoSignup = () => {
     navigate("/member/signup");
   }
+
   return (
     <div className="landing-page">
       <div className="content">
         <div className="start-logo-container">
           <img src="/image/Logo.png" alt="Logo" className="logo" />
         </div>
-        <div className={`button-container ${showButtons ? 'show' : ''}`}>
-          <button className="landing-button" onClick={GotoLogin}>
-            로그인
-          </button>
-          <button className="landing-button" onClick={GotoSignup}>
-            회원가입
-          </button>
-        </div>
+      </div>
+      <div className={`button-container ${showButtons ? 'show' : ''}`}>
+        <button className="landing-button" onClick={GotoLogin}>
+          로그인
+        </button>
+        <button className="landing-button" onClick={GotoSignup}>
+          회원가입
+        </button>
       </div>
     </div>
   );
