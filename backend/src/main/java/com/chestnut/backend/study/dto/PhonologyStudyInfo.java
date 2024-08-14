d3990f5412d1d7a1e1757fefabe050b51ed0785d
Elenaljh <hye9807@naver.com>
2024-08-14 13:13:32 +0900

    feature: 학습 api 수정 (4단원 학습 이력 추가)

--- backend/src/main/java/com/chestnut/backend/study/dto/PhonologyStudyInfo.java
+++ backend/src/main/java/com/chestnut/backend/study/dto/PhonologyStudyInfo.java
@@ -3,16 +3,27 @@ package com.chestnut.backend.study.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @ToString
 @NoArgsConstructor
-@AllArgsConstructor
 public class PhonologyStudyInfo {
     private Byte studyCategoryId;
     private Long studyId;
     private String phonologyRule;
     private String phonologyExplanation;
     private String example;
+    @Setter
+    private int isPass;
+
+    public PhonologyStudyInfo(Byte studyCategoryId, Long studyId, String phonologyRule, String phonologyExplanation, String example) {
+        this.studyCategoryId = studyCategoryId;
+        this.studyId = studyId;
+        this.phonologyRule = phonologyRule;
+        this.phonologyExplanation = phonologyExplanation;
+        this.example = example;
+    }
+
 }
