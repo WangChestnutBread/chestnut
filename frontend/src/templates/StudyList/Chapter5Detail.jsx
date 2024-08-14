61962dfbd9bed912f04e2965efcc26d59a63a9cf
김시현 <gus3260@naver.com>
2024-08-14 15:16:26 +0900

    fix:학습페이지 정답페일 수정

--- frontend/src/templates/StudyList/Chapter5Detail.jsx
+++ frontend/src/templates/StudyList/Chapter5Detail.jsx
@@ -1,19 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import StudyBackButton from "../../molecules/StudyBackButton";
 import ChestNutButton from "../../organisms/ChestNutButton";
-import Notation from "../../organisms/StudyList/NotationChapter1";
 import CameraOrganism from "../../organisms/StudyList/CameraOrganism";
 import RecordData from "../../organisms/StudyList/Record";
 import { useParams } from "react-router-dom";
 import baseApi from "../../api/fetchAPI";
 import Ch5SM from "../../organisms/StudyList/Ch5SM";
-import Ch5Pronunciation from "../../organisms/StudyList/Ch5Pronunciation"
+import Ch5Pronunciation from "../../organisms/StudyList/Ch5Pronunciation";
+import Ch3Notation from "./../../organisms/StudyList/Ch3Notaion";
+import Lottie from "lottie-react";
+import Correct from "../../assets/lottie/correct.json";
+import Wrong from "../../assets/lottie/wrong.json"
 
 const Chapter5Detail = () => {
   const params = useParams();
-  const [realData, setRealData] = useState("내발음😎");
+  const [realData, setRealData] = useState("내발음");
   const [answerData, setAnswerData] = useState([100000]);
-  const [selectedChar, setSelectedChar] = useState("");  // 클릭된 글자 상태 관리
+  const [selectedChar, setSelectedChar] = useState(""); // 클릭된 글자 상태 관리
+  const [show, isShow] = useState(false);
+  const [correct, isCorrect] = useState(false);
+  const [wrong, isWrong] = useState(false)
+
+  useEffect(() => {
+    isShow(true);
+  }, [selectedChar]);
 
   const moveData = (value) => {
     setRealData(value);
@@ -22,14 +32,25 @@ const Chapter5Detail = () => {
   const answer = (value) => {
     setAnswerData(value);
     if (value.length === 0) {
-      baseApi.get('/log/study',{
-        params:{
-          studyId: params.studyId,
-          isPass: 1
-        }
-      }).then((res) => {
-        alert('축하드려요 성공입니다.');
-      });
+      baseApi
+        .get("/log/study", {
+          params: {
+            studyId: params.studyId,
+            isPass: 1,
+          },
+        })
+        .then((res) => {
+          isCorrect(true);
+          setTimeout(() => {
+            isCorrect(false);
+          }, 2000);
+        }).catch((err) => {
+          isWrong(true);
+
+          setTimeout(() => {
+            isWrong(false)
+          },2000)
+        })
     }
   };
 
@@ -44,37 +65,65 @@ const Chapter5Detail = () => {
           </div>
         </div>
       </div>
-      {/* 표기, 카메라 */}
+      {/* 표기, 입모양, 혀모양 */}
       <div className="container">
         <div className="row">
-          <div className="col-6 mt-2">
-            <Notation word={params} realData={realData}/>
+          <div className="col-8 mt-2">
+            <div className="d-flex">
+              <div className="col-4">
+                {/* <Notation word={params} /> */}
+                <Ch3Notation word={params} />
+              </div>
+              <div className="col-8">
+                <CameraOrganism />
+              </div>
+            </div>
+
+            <div className="mt-2 justify-content-center">
+              <Ch5Pronunciation
+                saying={params}
+                realData={realData}
+                location={answerData}
+                onCharacterClick={setSelectedChar}
+              />
+            </div>
+            <div className="mt-2">
+              <RecordData func={moveData} func2={answer} />
+              <div
+                style={{
+                  position: "fixed",
+                  top: "50%",
+                  left: "50%",
+                  zIndex: 1000,
+                  width: "800px",
+                  height: "800px",
+                  transform: "translate(-50%, -50%)", // 화면 중앙에 위치시키기 위해
+                  pointerEvents: "none", // 이 요소는 클릭을 무시하도록 설정
+                }}
+              >
+                {correct ? <Lottie animationData={Correct} /> : <></>}
+              </div>
+              <div style={{
+                  position: "fixed",
+                  top: "50%",
+                  left: "50%",
+                  zIndex: 1000,
+                  width: "800px",
+                  height: "800px",
+                  transform: "translate(-50%, -50%)", // 화면 중앙에 위치시키기 위해
+                  pointerEvents: "none", // 이 요소는 클릭을 무시하도록 설정
+                }}>
+                   {wrong ? <Lottie animationData={Wrong} /> : <></>}
+              </div>
+            </div>
           </div>
-          <div className="col-6 mt-2">
-            <CameraOrganism />
+          <div className="col-4 mt-2">
+            <Ch5SM hangeul={params} selectedChar={selectedChar} />
           </div>
         </div>
-        {/* 소리나는 방법, ???(우승다람쥐) */}
-        <div className="row">
-          <div className="col-6 mt-2" >
-            <Ch5Pronunciation 
-              saying={params} 
-              realData={realData} 
-              location={answerData}
-              onCharacterClick={setSelectedChar}  // 클릭된 글자를 전달
-            />
-          </div>
-          <div className="col-6 mt-2 mb-3" style={{"width":"500px", 'height': "350px"}} >
-            <img src="/image/success.png" alt="practice" style={{"width": "100%", "height":"100%"}} />
-          </div>
-          <div>
-            <Ch5SM hangeul={params} selectedChar={selectedChar}/>  {/* 선택된 글자 전달 */}
-          </div>
-        </div>
+        {/* 소리나는 방법, 카메라 */}
+
         {/* 마이크 */}
-        <div className="mt-5">
-        <RecordData func={moveData} func2={answer}/>
-        </div>
       </div>
     </div>
   );
--- frontend/src/templates/StudyList/Chapter6Detail.jsx
+++ frontend/src/templates/StudyList/Chapter6Detail.jsx
@@ -1,19 +1,26 @@
 import "../NavbarExample.css";
 import StudyBackButton from "../../molecules/StudyBackButton";
 import ChestNutButton from "../../organisms/ChestNutButton";
-import Notation from "../../organisms/StudyList/NotationChapter1";
 import CameraOrganism from "../../organisms/StudyList/CameraOrganism";
 import RecordData from "../../organisms/StudyList/Record";
-import Pronunciation from "../../organisms/StudyList/Pronunciations";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import baseApi from "../../api/fetchAPI";
+import Ch3Notation from "./../../organisms/StudyList/Ch3Notaion";
+import Ch5Pronunciation from "./../../organisms/StudyList/Ch5Pronunciation";
+import Ch5SM from "./../../organisms/StudyList/Ch5SM";
+import Lottie from "lottie-react";
+import Correct from "../../assets/lottie/correct.json";
+import Wrong from "../../assets/lottie/wrong.json";
 
 const Chapter6Detail = () => {
   const params = useParams();
-  const [realData, setRealData] = useState("내발음😎");
+  const [realData, setRealData] = useState("녹음된 발음");
   const [answerData, setAnswerData] = useState([100000]);
   const [show, isShow] = useState(false);
+  const [selectedChar, setSelectedChar] = useState("");
+  const [correct, isCorrect] = useState(false);
+  const [wrong, isWrong] = useState(false);
 
   const moveData = (value) => {
     setRealData(value);
@@ -31,8 +38,16 @@ const Chapter6Detail = () => {
           },
         })
         .then((res) => {
-          console.log(res);
-          console.log('축하드려요 성공입니다.');
+          isCorrect(true);
+          setTimeout(() => {
+            isCorrect(false);
+          }, 2000);
+        })
+        .catch((err) => {
+          isWrong(true);
+          setTimeout(() => {
+            isWrong(false);
+          }, 2000);
         });
     }
   };
@@ -48,33 +63,69 @@ const Chapter6Detail = () => {
           </div>
         </div>
       </div>
-      {/* 표기, 카메라 */}
+      {/* 표기, 입모양, 혀모양 */}
       <div className="container">
         <div className="row">
-          <div className="col-6 mt-2">
-            <Notation word={params} />
+          <div className="col-8 mt-2">
+            <div className="d-flex">
+              <div className="col-4">
+                {/* <Notation word={params} /> */}
+                <Ch3Notation word={params} />
+              </div>
+              <div className="col-8">
+                <CameraOrganism />
+              </div>
+            </div>
+
+            <div className="mt-2 justify-content-center">
+              <Ch5Pronunciation
+                saying={params}
+                realData={realData}
+                location={answerData}
+                onCharacterClick={setSelectedChar}
+              />
+            </div>
+            <div className="mt-2">
+              <RecordData func={moveData} func2={answer} />
+            </div>
           </div>
-          <div className="col-6 mt-2">
-            <CameraOrganism />
+          <div className="col-4 mt-2">
+            <Ch5SM hangeul={params} selectedChar={selectedChar} />
           </div>
         </div>
-        {/* 소리나는 방법, ???(우승다람쥐) */}
+        {/* 소리나는 방법, 카메라 */}
         <div className="row">
-          <div className="col-6 mt-2">
-            <Pronunciation
-              saying={params}
-              realData={realData}
-              location={answerData}
-            />
-          </div>
-          <div className="col-6 mt-2 mb-3" style={{"width":"500px", 'height': "350px"}} >
-            <img src="/image/success.png" alt="practice" style={{"width": "100%", "height":"100%"}} />
-          </div>
+          <div className="col-6 mt-2 mb-3"></div>
+        </div>
+        <div
+          style={{
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            zIndex: 1000,
+            width: "800px",
+            height: "800px",
+            transform: "translate(-50%, -50%)", // 화면 중앙에 위치시키기 위해
+            pointerEvents: "none", // 이 요소는 클릭을 무시하도록 설정
+          }}
+        >
+          {correct ? <Lottie animationData={Correct} /> : <></>}
+        </div>
+        <div
+          style={{
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            zIndex: 1000,
+            width: "800px",
+            height: "800px",
+            transform: "translate(-50%, -50%)", // 화면 중앙에 위치시키기 위해
+            pointerEvents: "none", // 이 요소는 클릭을 무시하도록 설정
+          }}
+        >
+          {wrong ? <Lottie animationData={Wrong} /> : <></>}
         </div>
         {/* 마이크 */}
-        <div className="mt-5">
-        <RecordData func={moveData} func2={answer} />
-        </div>
       </div>
     </div>
   );
