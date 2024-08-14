fdf4351cdfbaaa53593a6694e1e2aab5c6c7949f
Elenaljh <hye9807@naver.com>
2024-08-14 20:52:23 +0900

    feat: 날짜별 학습기록 조회 기능 개발

--- backend/src/main/java/com/chestnut/backend/log/controller/LogController.java
+++ backend/src/main/java/com/chestnut/backend/log/controller/LogController.java
@@ -3,6 +3,7 @@ package com.chestnut.backend.log.controller;
 import com.chestnut.backend.common.dto.ResponseDto;
 import com.chestnut.backend.log.service.LogService;
 import com.chestnut.backend.member.dto.CustomMemberDetails;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,19 @@ public class LogController {
         String loginId = customMemberDetails.getLoginId();
         logService.saveStudyLog(loginId, studyId, isPass == 1);
         ResponseDto<Object> result = new ResponseDto<>("200", null);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/word-list")
+    public ResponseEntity<?> getWordList(
+        @AuthenticationPrincipal CustomMemberDetails customMemberDetails,
+        @RequestParam("year") int year,
+        @RequestParam("month") int month,
+        @RequestParam("day") int day
+    ) {
+        String loginId = customMemberDetails.getLoginId();
+        List<String> studiedWordList = logService.getStudiedWordList(loginId, year, month, day);
+        ResponseDto<List<String>> result = new ResponseDto<>("200", studiedWordList);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
