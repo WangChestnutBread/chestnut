8c6d47c0bff73960c1ef1fc5906903cd36e11e73
김시현 <gus3260@naver.com>
2024-08-15 14:51:05 +0900

    fix: 단어장추가, 디테일 추가

--- frontend/src/organisms/StudyList/Ch3Notaion.js
+++ frontend/src/organisms/StudyList/Ch3Notaion.js
@@ -3,11 +3,13 @@ import baseApi from "../../api/fetchAPI";
 import "./Ch3Notation.css";
 
 const Ch3Notation = ({word}) => {
-  
+  console.log(word);
   const [data, setData] = useState()
+  const [pronounce, setPronounce] = useState()
   useEffect(() => {
     baseApi.get(`/study/detail/${word.studyId}/word`).then((res) => {
       setData(res.data.data.word)
+      setPronounce(res.data.data.pronounce)
 
     })
   },[word.studyId])
@@ -17,9 +19,9 @@ const Ch3Notation = ({word}) => {
 
   return (
     <div className="ch3box rounded" style={{width:"300px"}}>
-      {parseInt(word.chapterId) === 1 ? <div className="abc">표기[발음]</div> : <div className="abc">표기</div>}
+      {parseInt(word.chapterId) !== 6 ? <div className="abc">표기[발음]</div> : <div className="abc">표기</div>}
       <div className="data1">{data}</div>
-      {parseInt(word.chapterId) === 1 ? <div className="data2">{`[${data}]`}</div>: <></>}
+      {parseInt(word.chapterId) !== 6 ? <div className="data2">{`[${pronounce}]`}</div>: <></>}
     </div>
   );
 };
