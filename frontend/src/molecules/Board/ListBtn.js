46dd54d1c1af91e46daa78bbf4f5b7faa47f2b44
김아름 <rdf136@naver.com>
2024-08-15 14:28:18 +0900

    design: 게시판 목록 버튼명 - 각 게시판(공지사항/QnA)으로 수정

--- frontend/src/molecules/Board/ListBtn.js
+++ frontend/src/molecules/Board/ListBtn.js
@@ -11,7 +11,7 @@ const ListBtn = () => {
 
   return (
     <div className="d-flex justify-content-start mb-2 mt-5">
-      <button className="list-btn" onClick={handleQnaClick} style={{border:"none", width: "100px", height: "45px"}}>게시판 목록</button>
+      <button className="list-btn" onClick={handleQnaClick} style={{border:"none", width: "100px", height: "45px"}}>QnA 목록</button>
     </div>
   );
 };
